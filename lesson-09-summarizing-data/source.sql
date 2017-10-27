f99607aaba61d620868d459f950cba14348e5c92
nhannguyen95 <nhannguyenmath95@gmail.com>
2017-10-27 13:07:49 +0700

    add code for combining aggregate functions

--- lesson-09-summarizing-data/source.sql
+++ lesson-09-summarizing-data/source.sql
@@ -32,6 +32,13 @@ FROM OrderItems
 WHERE order_num = 20005;
 
 -- Aggregates on distinct values
-SELECT AVG(DISTINCT prod_price) AS avg_price 
+SELECT AVG(DISTINCT prod_price) AS avg_price
 FROM Products
 WHERE vend_id = 'DLL01';
+
+-- Combining Aggregate Functions
+SELECT COUNT(*) AS num_items, 
+       MIN(prod_price) AS price_min,
+       MAX(prod_price) AS price_max,
+       AVG(prod_price) AS price_avg
+FROM Products;
